871395b73bbdb29d3d9e5f9f94341f5ef1213e6f
DD Engineer <48682076+DataDrivenEngineer@users.noreply.github.com>
2024-01-26 22:49:02 -0800

    Confirmed image read correctly by writing it back to png

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,21 @@ using namespace std;
 int main(int argc, char **argv)
 {
   Mat img = imread(string("data/gray.png"), IMREAD_UNCHANGED);
+
+  vector<int> compression_params;
+  compression_params.push_back(IMWRITE_PNG_COMPRESSION);
+  compression_params.push_back(9);
+
+  bool result = imwrite("data/gray_test.png", img, compression_params);
+
+  if (result)
+  {
+    printf("Saved PNG file with alpha data.\n");
+  }
+  else
+  {
+    printf("ERROR: Can't save PNG file.\n");
+  }
+
   return 0;
 }
